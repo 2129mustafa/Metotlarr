a083090c85e805cf32f92cef71be94ceb9c2b9d9
MUSTAFA YENAL <202803018@ogr.cbu.edu.tr>
2021-03-31 18:16:26 +0300

    Add project files.

---
+++ Metotlarr/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Metotlarr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("------------------------");
+            }
+            Console.WriteLine("--------------- Metotlar ---------------");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Add(urun1);
+            sepetManager.Add(urun2);
+
+
+            sepetManager.Add2("Armut", "Yeşil armut", 12, 10);
+            sepetManager.Add2("Elma", "Yeşil Elma", 12, 9);
+            sepetManager.Add2("Karpuz", "Diyarbakır Karpuzu", 12, 8);
+           
+        }
+    }
+}
---
+++ Metotlarr/SepetManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlarr
+{
+    //operasyon
+    class SepetManager
+    {
+        public void Add(Urun urun) //burya ekleyeceğimiz urunun prametrseni gönderdik.
+        {
+            Console.WriteLine("Tebrikler.Sepete Eklendi : "+urun.Adi);
+        }
+        public void Add2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler.Sepete eklendi: "+urunAdi);
+        }
+    }
+}
